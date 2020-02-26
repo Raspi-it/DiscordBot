000d11e906657a5541d4e8c79980ea3c6fd4620c
Raspi-it <59459856+Raspi-it@users.noreply.github.com>
2020-02-26 19:56:34 +0100

    Add file to repository

---
+++ bot.js
@@ -0,0 +1,23 @@
+// Run dotenv
+require('dotenv').config();
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Moin') {
+    msg.reply('Halts Maul!');
+  }
+});
